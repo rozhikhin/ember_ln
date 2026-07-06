d81cc18696a6e01d00a5f1760c323a900c35ec53
Alexander ROzhikhin <rozhihin@gmail.com>
2026-07-06 15:29:44 +0300

    Add bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    'ember-power-select': {
+      theme: 'bootstrap'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +21,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('vendor/bootstrap/dist/css/bootstrap.min.css');
 
   return app.toTree();
 };
